8dfe0288a63d309d199a77a238c61c1ecbb9a700
打盹的鱼 <nlxuran@163.com>
2019-02-15 17:32:14 +0800

    数据库设计

---
+++ database/migrations/2019_02_15_091313_create_novels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNovelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('novels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 100)->index();
+            $table->string('desc', 255);
+            $table->string('author', 16);
+            $table->string('novel_type', 16);//小说类型
+            $table->string('tags', 255);//小说标签
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('novels');
+    }
+}
---
+++ database/migrations/2019_02_15_092352_create_chapter_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChapterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapter', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->integer('novel_id');
+            $table->integer('content_id');
+            $table->tinyInteger('is_delete');
+            $table->integer('words');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapter');
+    }
+}
