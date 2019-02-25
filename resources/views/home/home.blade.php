acb3ffff398fc521e700d07c6a8b31d077302155
打盹的鱼 <nlxuran@163.com>
2019-02-25 17:13:10 +0800

    切割body中左侧和右侧的视图

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -3,13 +3,11 @@
     <div class="row">
         <div class="col-sm-8 border border-color-1 rounded d-block bg-white">
             <div class="row bg-light">
-                <h4 class="col-xs-4" style="margin-top:8px;margin-left: 10px;"><i class="fa fa-diamond fa-fw"></i>本站推荐
-                </h4>
+                <h4 class="col-xs-4" style="margin-top:8px;margin-left: 10px;"><i class="fa fa-diamond fa-fw"></i>本站推荐</h4>
                 <div class="col-md-6 col-xs-8 ml-auto p-1">
                     <form action="">
                         <div class="input-group">
-                            <input type="text" class="form-control" placeholder="请输入您需要搜索的关键字" id="bdcsMain"
-                                   name="searchkey">
+                            <input type="text" class="form-control" placeholder="请输入您需要搜索的关键字" id="bdcsMain" name="searchkey">
                             <span class="input-group-btn">
                             <button class="btn btn-outline-success" type="submit">搜索</button>
                         </span>
@@ -18,111 +16,9 @@
                 </div>
             </div>
             <div class="row p-3">
-                <div class="col-md-3 col-xs-6 mt-3">
-                    <a class="thumbnail border border-color-1 rounded d-block"
-                       href="http://www.woyaodu.com/xiaoshuo/0/1/" title="二婚甜蜜蜜">
-                        <img src="http://www.woyaodu.com/files/article/image/0/2/2s.jpg" style="height:182px;">
-                        <div class="caption">
-                            <strong>二婚甜蜜蜜</strong>
-                            <br>
-                            <span class="text-muted fs-12">丁念曹子.. / 著</span>
-                            <br>
-                            <span class="text-info fs-12">46244人在看</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-3 col-xs-6 mt-3">
-                    <a class="thumbnail border border-color-1 rounded d-block"
-                       href="http://www.woyaodu.com/xiaoshuo/0/1/" title="二婚甜蜜蜜">
-                        <img src="http://www.woyaodu.com/files/article/image/0/2/2s.jpg" style="height:182px;">
-                        <div class="caption">
-                            <strong>二婚甜蜜蜜</strong>
-                            <br>
-                            <span class="text-muted fs-12">丁念曹子.. / 著</span>
-                            <br>
-                            <span class="text-info fs-12">46244人在看</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-3 col-xs-6 mt-3">
-                    <a class="thumbnail border border-color-1 rounded d-block"
-                       href="http://www.woyaodu.com/xiaoshuo/0/1/" title="二婚甜蜜蜜">
-                        <img src="http://www.woyaodu.com/files/article/image/0/2/2s.jpg" style="height:182px;">
-                        <div class="caption">
-                            <strong>二婚甜蜜蜜</strong>
-                            <br>
-                            <span class="text-muted fs-12">丁念曹子.. / 著</span>
-                            <br>
-                            <span class="text-info fs-12">46244人在看</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-3 col-xs-6 mt-3">
-                    <a class="thumbnail border border-color-1 rounded d-block"
-                       href="http://www.woyaodu.com/xiaoshuo/0/1/" title="二婚甜蜜蜜">
-                        <img src="http://www.woyaodu.com/files/article/image/0/2/2s.jpg" style="height:182px;">
-                        <div class="caption">
-                            <strong>二婚甜蜜蜜</strong>
-                            <br>
-                            <span class="text-muted fs-12">丁念曹子.. / 著</span>
-                            <br>
-                            <span class="text-info fs-12">46244人在看</span>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-md-3 col-xs-6 mt-3">
-                    <a class="thumbnail border border-color-1 rounded d-block"
-                       href="http://www.woyaodu.com/xiaoshuo/0/1/" title="二婚甜蜜蜜">
-                        <img src="http://www.woyaodu.com/files/article/image/0/2/2s.jpg" style="height:182px;">
-                        <div class="caption">
-                            <strong>二婚甜蜜蜜</strong>
-                            <br>
-                            <span class="text-muted fs-12">丁念曹子.. / 著</span>
-                            <br>
-                            <span class="text-info fs-12">46244人在看</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-3 col-xs-6 mt-3">
-                    <a class="thumbnail border border-color-1 rounded d-block"
-                       href="http://www.woyaodu.com/xiaoshuo/0/1/" title="二婚甜蜜蜜">
-                        <img src="http://www.woyaodu.com/files/article/image/0/2/2s.jpg" style="height:182px;">
-                        <div class="caption">
-                            <strong>二婚甜蜜蜜</strong>
-                            <br>
-                            <span class="text-muted fs-12">丁念曹子.. / 著</span>
-                            <br>
-                            <span class="text-info fs-12">46244人在看</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-3 col-xs-6 mt-3">
-                    <a class="thumbnail border border-color-1 rounded d-block"
-                       href="http://www.woyaodu.com/xiaoshuo/0/1/" title="二婚甜蜜蜜">
-                        <img src="http://www.woyaodu.com/files/article/image/0/2/2s.jpg" style="height:182px;">
-                        <div class="caption">
-                            <strong>二婚甜蜜蜜</strong>
-                            <br>
-                            <span class="text-muted fs-12">丁念曹子.. / 著</span>
-                            <br>
-                            <span class="text-info fs-12">46244人在看</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-3 col-xs-6 mt-3">
-                    <a class="thumbnail border border-color-1 rounded d-block"
-                       href="http://www.woyaodu.com/xiaoshuo/0/1/" title="二婚甜蜜蜜">
-                        <img src="http://www.woyaodu.com/files/article/image/0/2/2s.jpg" style="height:182px;">
-                        <div class="caption">
-                            <strong>二婚甜蜜蜜</strong>
-                            <br>
-                            <span class="text-muted fs-12">丁念曹子.. / 著</span>
-                            <br>
-                            <span class="text-info fs-12">46244人在看</span>
-                        </div>
-                    </a>
-                </div>
+                @for($a = 0; $a < 8; $a++)
+                    @include('shared._left_novel')
+                @endfor
             </div>
         </div>
         <div class="col-sm-4 pl-4">
@@ -142,88 +38,19 @@
             </ul>
             <div class="row-sm-12 tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-success">1</span>
-                        <span class="d-inline-block text-truncate" style="max-width: 248px;vertical-align: middle;">
-                            <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我我我我我我我我我我我">我我我我我按我我我我我</a>
-                            <small class="text-muted">/ 我我我我asdsadad</small>
-                        </span>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-success">2</span>
-                        <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我与空姐的荒岛求生记">我与空姐的荒岛求生记</a>
-                        <small class="text-muted">/ 张飞宁小秋小说</small>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-success">3</span>
-                        <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我与空姐的荒岛求生记">我与空姐的荒岛求生记</a>
-                        <small class="text-muted">/ 张飞宁小秋小说</small>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-secondary">4</span>
-                        <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我与空姐的荒岛求生记">我与空姐的荒岛求生记</a>
-                        <small class="text-muted">/ 张飞宁小秋小说</small>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
+                    @for($a = 0; $a < 15; $a++)
+                        @include('shared._right_novel', ['key' => $a])
+                    @endfor
                 </div>
                 <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-success">1</span>
-                        <span class="d-inline-block text-truncate" style="max-width: 248px;vertical-align: middle;">
-                            <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="你你你你你你你你你">你你你你你你你你你你你</a>
-                            <small class="text-muted">/ 张飞宁小秋小说</small>
-                        </span>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-success">2</span>
-                        <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我与空姐的荒岛求生记">我与空姐的荒岛求生记</a>
-                        <small class="text-muted">/ 张飞宁小秋小说</small>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-success">3</span>
-                        <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我与空姐的荒岛求生记">我与空姐的荒岛求生记</a>
-                        <small class="text-muted">/ 张飞宁小秋小说</small>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-secondary">4</span>
-                        <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我与空姐的荒岛求生记">我与空姐的荒岛求生记</a>
-                        <small class="text-muted">/ 张飞宁小秋小说</small>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
+                    @for($a = 0; $a < 12; $a++)
+                        @include('shared._right_novel', ['key' => $a])
+                    @endfor
                 </div>
                 <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-success">1</span>
-                        <span class="d-inline-block text-truncate" style="max-width: 248px;vertical-align: middle;">
-                            <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="她">她她她她她她她她她她她</a>
-                            <small class="text-muted">/ 张飞宁小秋小说</small>
-                        </span>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-success">2</span>
-                        <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我与空姐的荒岛求生记">我与空姐的荒岛求生记</a>
-                        <small class="text-muted">/ 张飞宁小秋小说</small>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-success">3</span>
-                        <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我与空姐的荒岛求生记">我与空姐的荒岛求生记</a>
-                        <small class="text-muted">/ 张飞宁小秋小说</small>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
-                    <li class="list-group-item p-2">
-                        <span class="badge badge-pill badge-secondary">4</span>
-                        <a href="http://www.woyaodu.com/xiaoshuo/33/33590/" title="我与空姐的荒岛求生记">我与空姐的荒岛求生记</a>
-                        <small class="text-muted">/ 张飞宁小秋小说</small>
-                        <span class="float-right text-danger">82576</span>
-                    </li>
+                    @for($a = 0; $a < 10; $a++)
+                        @include('shared._right_novel', ['key' => $a])
+                    @endfor
                 </div>
             </div>
         </div>
---
+++ resources/views/shared/_left_novel.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-3 col-xs-6 mt-3">
+    <a class="thumbnail border border-color-1 rounded d-block"
+       href="http://www.woyaodu.com/xiaoshuo/0/1/" title="二婚甜蜜蜜">
+        <img src="http://www.woyaodu.com/files/article/image/0/2/2s.jpg" style="height:182px;">
+        <div class="caption">
+            <strong>二婚甜蜜蜜</strong>
+            <br>
+            <span class="text-muted fs-12">丁念曹子.. / 著</span>
+            <br>
+            <span class="text-info fs-12">46244人在看</span>
+        </div>
+    </a>
+</div>
